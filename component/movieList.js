f8756a6b40b3255059b6a5beb2cd1802635163f4
sho-r25 <tori.ushi12@gmail.com>
2020-12-17 00:01:17 +0900

    first commit

---
+++ component/movieList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+
+    shorten = (text,maxLength) => {
+        if (text && text.length >= maxLength){
+            return text.substr(0,maxLength) + '...'
+        }
+        return text
+    }
+
+	renderMovies(movies) {
+
+        const moveElements = movies.map((movie) => (
+            <div className='col-lg-4 col-md-6 mb-4'>
+                <div className='card h-100'>
+                    <a href='#'>
+                        <img className='card-img-top' src={movie.image} alt='' />
+                    </a>
+                    <div className='card-body'>
+                        <h4 className='card-title'>
+                            <a href='#'>{movie.name}</a>
+                        </h4>
+                        <p className='card-text'>{this.shorten(movie.description,100)}</p>
+                    </div>
+                    <div className='card-footer'>
+                        <small className='text-muted'>{movie.rating}</small>
+                    </div>
+                </div>
+            </div>
+        ))
+        return moveElements
+	}
+
+	render() {
+        const { movies } = this.props;
+		return (
+			<React.Fragment>
+				{this.renderMovies(movies) }
+			</React.Fragment>
+		);
+	}
+}
+
+export default MovieList;
---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+
+// const About = () => {
+// 	const message = "hello world!!";
+// 	return <h1>about - {message} </h1>;
+// };
+
+// const About = () => {
+//     const message = "hello world!!"; 
+// 	return React.createElement('h1',null,'I am sho')
+// };
+
+
+
+// function About(){
+//     return (
+//         <h1>hello about</h1>
+//     )
+// }
+
+
+
+class About extends React.Component {
+
+    render(){
+        return(<p>sf</p>)
+    }
+}
+
+export default About;
